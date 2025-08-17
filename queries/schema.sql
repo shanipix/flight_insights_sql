a7a2db3a6af02f5837d405a8d6d4a010a1951c3a
shanipix <dahloum.sha@gmail.com>
2025-08-17 22:19:38 +0200

    Create schema.sql

---
+++ queries/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS flights_raw;
+
+CREATE TABLE IF NOT EXISTS flights_raw (
+  index INTEGER,
+  airline TEXT,
+  flight TEXT,
+  source_city TEXT,
+  departure_time TEXT,
+  stops INTEGER,
+  arrival_time TEXT,
+  destination_city TEXT,
+  class TEXT,
+  duration TEXT,
+  days_left INTEGER,
+  price REAL
+);
